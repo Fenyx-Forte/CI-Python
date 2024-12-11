c1e8552fed6f98827e0d391258eec44d69d3ed86
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-11 15:08:35 -0300

    build: adicionando __init__.py do pacote src

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""src.
+
+Um repositório Python para testar configurações específicas de CI do pre-commit e do
+GitHub Actions.
+
+Este pacote é composto por 1 submódulo que fornece funcionalidades específicas para
+operações matemáticas. O módulo principal "calc" inclui funções para adição, subtração,
+multiplicação e divisão.
+
+Componentes:
+    calc: Um pacote com ferramentas para operações matemáticas.
+"""
