48e2101362ebe28e0b109863e9ef0867a0ea0626
Fenyx Forte <fenyx.studies@gmail.com>
2024-12-11 15:08:12 -0300

    build: adicionando __init__.py

---
+++ src/calc/__init__.py
@@ -0,0 +1,14 @@
+"""calc.
+
+Um pacote para operações matemáticas básicas.
+
+Este pacote fornece funcionalidades para realizar cálculos matemáticos simples,
+organizados no módulo "calc". Atualmente, suporta as quatro operações básicas:
+- adição
+- subtração
+- multiplicação
+- divisão
+
+Módulos:
+    calc: Contém as funções para realizar as 4 operações matemáticas básicas.
+"""
